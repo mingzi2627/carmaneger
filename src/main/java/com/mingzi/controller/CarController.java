47c3c92c9ce5d37034aae7df77231618d9a87dd1
t184duyueqing <3051832751@qq.com>
2020-05-07 19:49:26 +0800

    2020年5月7日19:48:39提交一

--- src/main/java/com/mingzi/controller/CarController.java
+++ src/main/java/com/mingzi/controller/CarController.java
@@ -3,6 +3,7 @@ package com.mingzi.controller;
 import com.mingzi.pojo.Car;
 import com.mingzi.service.CarService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -13,6 +14,7 @@ import java.util.List;
  * @create 2020-05-07 16:13
  */
 @RestController
+@RequestMapping("/sys/car")
 public class CarController {
     @Resource
     private CarService carService;
